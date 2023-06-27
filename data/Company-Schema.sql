30c266ef2ed73ede6fe244c8788f72a49ab9ba55
Nweekley84 <nweekley27@gmail.com>
2023-06-27 16:21:47 -0700

    :card_file_box: Edited the Company-Schema.sql file, it was giving syntax errors

--- data/Company-Schema.sql
+++ data/Company-Schema.sql
@@ -3,27 +3,26 @@ CREATE DATABASE company_DB;
 USE company_DB;
 
 CREATE TABLE departments (
-    id INT NOT NULL AUTO_INCREMENT
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id)    
 );
-
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(9,2),
     department_id INT,
     PRIMARY KEY (id),
-    FOREIGN KEY (departent_id) REFERENCES departments(id)
+    FOREIGN KEY (department_id) REFERENCES departments(id)
 );
-
 CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
+    manager_id INT,
     PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES roles(id)
+    FOREIGN KEY (role_id) REFERENCES roles(id),
     FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
 
